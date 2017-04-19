c9b669561e6fd9520605310a9d3a88f9a29f70e4
CorningSun <corningsun@163.com>
2017-04-19 22:25:04 +0800

    ?参数

--- src/main/java/com/girl/HelloController.java
+++ src/main/java/com/girl/HelloController.java
@@ -1,9 +1,9 @@
 package com.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,8 +16,8 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/say/{id}", method = RequestMethod.GET)
-    public String say(@PathVariable("id") Integer id) {
+    @RequestMapping(value = "/say", method = RequestMethod.GET)
+    public String say(@RequestParam("id") Integer id) {
         return "id: " + id;
     }
 }
