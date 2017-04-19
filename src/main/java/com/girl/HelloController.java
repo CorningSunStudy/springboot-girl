df969b422fabbfcd7f2be4f90c2b336cc4547c74
CorningSun <corningsun@163.com>
2017-04-19 22:09:43 +0800

    @RestController 等同于@Controller + @ResponseBody

--- src/main/java/com/girl/HelloController.java
+++ src/main/java/com/girl/HelloController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by corning on 2017/4/19.
  */
 @Controller
+@ResponseBody
 public class HelloController {
 
     @Autowired
@@ -16,7 +18,6 @@ public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-//        return girlProperties.getCupSize();
-        return "index";
+        return girlProperties.getCupSize();
     }
 }
