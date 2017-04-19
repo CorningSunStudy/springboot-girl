ace158a75a68ac22e42d54e842e8ef046b8cd6c4
CorningSun <corningsun@163.com>
2017-04-19 22:07:45 +0800

    thymeleaf templates 实现

--- src/main/java/com/girl/HelloController.java
+++ src/main/java/com/girl/HelloController.java
@@ -1,15 +1,14 @@
 package com.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by corning on 2017/4/19.
  */
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
@@ -17,6 +16,7 @@ public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return girlProperties.getCupSize();
+//        return girlProperties.getCupSize();
+        return "index";
     }
 }
