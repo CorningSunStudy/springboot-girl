3ac5ee4275cf209cf41e28a60e06e5611dc534e8
CorningSun <corningsun@163.com>
2017-04-20 17:22:53 +0800

    2-3 使用AOP处理请求（下）

--- src/main/java/com/girl/aspect/HttpAspect.java
+++ src/main/java/com/girl/aspect/HttpAspect.java
@@ -1,12 +1,14 @@
 package com.girl.aspect;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by corning on 2017/4/20.
@@ -22,13 +24,22 @@ public class HttpAspect {
     }
 
     @Before("log()")
-    public void doBefore() {
-        logger.info("log HttpAspect Before log !!!!!!");
+    public void doBefore(JoinPoint joinPoint ) {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+
+        logger.info("method={}, url={}, ip={}", request.getMethod(), request.getRequestURL(), request.getRemoteAddr());
+        logger.info("class_method={}, args={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 
     @After("log()")
     public void doAfter() {
         logger.info("log HttpAspect After log !!!!!!");
+    }
+
+    @AfterReturning(value = "log()", returning = "object")
+    public void doAfterReturning(Object object) {
+        logger.info("response={}", object);
     }
 
 }
--- src/main/java/com/girl/domain/Girl.java
+++ src/main/java/com/girl/domain/Girl.java
@@ -51,4 +51,13 @@ public class Girl {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
