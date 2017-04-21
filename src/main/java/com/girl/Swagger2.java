f7e60a453add1f349c0c44c7d792fd0a708010fb
CorningSun <corningsun@163.com>
2017-04-21 12:25:51 +0800

    增加 Swagger API 支持

---
+++ src/main/java/com/girl/Swagger2.java
@@ -0,0 +1,43 @@
+package com.girl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by corning on 2017/4/21.
+ *
+ * 访问方式：http://localhost:8080/swagger-ui.html
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.girl.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("教程：http://blog.didispace.com/springbootswagger2/")
+                .termsOfServiceUrl("http://localhost:8080/swagger-ui.html")
+                .contact("contact")
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/girl/controller/HelloController.java
+++ src/main/java/com/girl/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.girl.controller;
 
 import com.girl.properties.GirlProperties;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +16,10 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
+    @ApiOperation(value = "你好")
+    @ApiImplicitParam(name = "id", value = "编号", dataType = "Integer")
     @GetMapping(value = "/say")
     public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
-        return "id: " + id;
+        return "你好！id: " + id;
     }
 }
